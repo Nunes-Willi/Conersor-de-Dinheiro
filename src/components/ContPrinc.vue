7ce1bc49829532651080c847c02470d4895e97c6
Nunes-Will <nuneswillroberto@gmail.com>
2022-05-06 19:42:31 -0300

    css vue

--- src/components/ContPrinc.vue
+++ src/components/ContPrinc.vue
@@ -6,5 +6,10 @@ export default {};
 <article class ="money">
     <input type="number" id="valor">
 </article>
+
+<article>
+    <input type="text" disabled id="moeda">
+</article>
+
 <button id="btnCalcular">Calcular</button>
 </template>
